7a36f3e3d8decb9e2f5556e566a1bda504add4b5
Danny Lin <danny@kdrag0n.dev>
2022-04-10 20:54:32 -0700

    Allow enabling recents overview grid on phones
    Change-Id: I610988f90d249c0c9284d89a160f2e0bf2f0b154

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -23,6 +23,7 @@ import static com.android.launcher3.folder.ClippedFolderIconLayoutRule.ICON_OVER
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Path;
@@ -49,6 +50,8 @@ import java.io.PrintWriter;
 
 @SuppressLint("NewApi")
 public class DeviceProfile {
+
+    public static final String KEY_PHONE_OVERVIEW_GRID = "pref_allow_phone_overview_grid";
 
     private static final int DEFAULT_DOT_SIZE = 100;
     // Ratio of empty space, qsb should take up to appear visually centered.
@@ -272,6 +275,7 @@ public class DeviceProfile {
         }
 
         hotseatQsbHeight = res.getDimensionPixelSize(R.dimen.qsb_widget_height);
+        SharedPreferences prefs = Utilities.getPrefs(context);
         boolean isTaskBarEnabled = LineageSettings.System.getInt(context.getContentResolver(),
                 LineageSettings.System.ENABLE_TASKBAR, isTablet ? 1 : 0) == 1;
         isTaskbarPresent = isTaskBarEnabled && ApiWrapper.TASKBAR_DRAWN_IN_PROCESS
@@ -359,7 +363,9 @@ public class DeviceProfile {
                 ? res.getDimensionPixelSize(R.dimen.scalable_grid_qsb_bottom_margin)
                 : 0;
 
-        overviewShowAsGrid = isTablet && FeatureFlags.ENABLE_OVERVIEW_GRID.get();
+        boolean allowPhoneOverviewGrid = prefs.getBoolean(KEY_PHONE_OVERVIEW_GRID, false);
+        overviewShowAsGrid = (isTablet || allowPhoneOverviewGrid) &&
+                FeatureFlags.ENABLE_OVERVIEW_GRID.get();
         overviewTaskMarginPx = overviewShowAsGrid
                 ? res.getDimensionPixelSize(R.dimen.overview_task_margin_focused)
                 : res.getDimensionPixelSize(R.dimen.overview_task_margin);
